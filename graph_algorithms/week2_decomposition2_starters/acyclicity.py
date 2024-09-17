36af1e269fa243ab2a93333c37dd5258b452e4e6
Edi Wijaya <edi.wijaya@danamas.co.id>
2024-09-17 12:17:30 +0700

    Clear up unnecessary code

--- graph_algorithms/week2_decomposition2_starters/acyclicity.py
+++ graph_algorithms/week2_decomposition2_starters/acyclicity.py
@@ -48,7 +48,6 @@ class GraphDirected:
     def acyclic(self, adj) -> int:
         self.cyclic = False
         self.assign_adj(adj)
-        # print(self.adj)
         for v in range(self.num_vertex):
             self.origin_vertex = v
             self.visited = [False] * self.num_vertex # to assign new cycle after each connectivity
@@ -74,21 +73,3 @@ if __name__ == '__main__':
 ### [[1], [2], [0], [0]]
 # python acyclicity.py <<< "5 7" <<< "1 2" <<< "2 3" <<< "1 3" <<< "3 4" <<< "1 4" <<< "2 5" <<< "3 5"
 ### [[1, 2, 3], [2, 4], [3, 4], [], []]
-
-
-
-    # def topological_sort(self, adj) -> int:
-    #     # If graph G contains cycle, it cannot be linearly ordered. Thus, if it is linearly ordered, it does not contains cycle (acyclic).
-    #     # A graph G is DAG if it has no cycles (acyclic).
-    #     # Any DAG can be linearly ordered.
-
-    #     # If is is a DAG, then it is acyclic.
-    #     # We can use is_dag to determine if it is cyclic or acyclic.
-
-    #     self.assign_adj(adj)
-    #     self.depth_first_search()
-    #     reverse_ordered_post = list(zip(self.post, [i for i in range(1, len(self.post) + 1)]))
-    #     print(reverse_ordered_post)
-    #     reverse_ordered_post = sorted(reverse_ordered_post, reverse=True)
-    #     reverse_ordered_post = [idx for rank, idx in reverse_ordered_post]
-    #     return reverse_ordered_post
